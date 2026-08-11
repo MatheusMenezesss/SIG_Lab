04f9293d46094d008e05341427abc700b9b17ee9
LIVE <live@MacBook-Pro-de-LIVE.local>
2026-08-11 16:30:00 -0300

    correcao nas pastas

---
+++ backend/API-siglab/src/main/java/br/com/siglab/API_siglab/domain/usuario/solicitacaoMapper.java
@@ -0,0 +1,5 @@
+package br.com.siglab.API_siglab.domain.usuario;
+
+public class solicitacaoMapper {
+    
+}
---
+++ backend/API-siglab/src/main/java/br/com/siglab/API_siglab/domain/usuario/solicitacaoService.java
@@ -0,0 +1,6 @@
+package br.com.siglab.API_siglab.domain.usuario;
+
+public class solicitacaoService {
+    
+    
+}
